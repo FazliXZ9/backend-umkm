86c205301c76184e63c8b2aa6e1c98a901b2f37b
FazliXZ9 <fazlia322sok@gmail.com>
2025-12-08 21:34:27 +0700

    finnal updt

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->brandName('ENP Admin')
+            ->brandName('~')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
