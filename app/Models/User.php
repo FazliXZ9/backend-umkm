5eeddaafc573c41548d2903efd59266115988ba0
root <root@web-vps-4.10.10.0.4>
2025-12-20 06:11:16 +0000

    Backup pertama backend-umkm ke Gitea

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,42 +7,38 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Filament\Models\Contracts\FilamentUser; // <--- TAMBAHKAN INI
+use Filament\Panel;                         // <--- TAMBAHKAN INI
+
+// 2. Tambahkan "implements FilamentUser"
+class User extends Authenticatable implements FilamentUser // <--- UBAH BARIS INI
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // 3. Tambahkan Fungsi Ini Paling Bawah
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Return true artinya: Izinkan user ini masuk ke panel admin
+        // Anda bisa tambah logika: return $this->email === 'admin@gmail.com';
+        return true; 
     }
 }
